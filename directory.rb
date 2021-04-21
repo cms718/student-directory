e4dfa214a30ed1eef03d6b6e5651034877be68cd
Charlie Slater <charliemslater1701@gmail.com>
2021-04-21 12:49:32 +0100

    Add interactive menu

--- directory.rb
+++ directory.rb
@@ -1,8 +1,6 @@
-def input_students 
+def input_students(students)
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
@@ -16,19 +14,37 @@ def input_students
   # return the array of students
   students
 end
-students = input_students
 def print_header
   puts "The students of the Villains Academy"
   puts "------------"
 end
 def print(students)
-  students.each do |student| 
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index| 
+    puts "#{index+1}.#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students(students)
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I'm not sure what you mean, please try again"
+    end
+  end
+end 
+interactive_menu
